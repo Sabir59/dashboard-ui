472773b7387a1ce8c5d2dd6b494fc489ac12abbc
William James <sabirnawaz59@gmail.com>
2024-04-30 18:17:25 +0500

    committed

--- src/app/services/reportSites.ts
+++ src/app/services/reportSites.ts
@@ -4,39 +4,352 @@ import { Injectable } from '@angular/core';
 export class ReportsService {
   private data = [
     {
-      id: 'cooking-reports',
+      id: '3182931',
       name: 'Cooking Reports',
-      description: 'Product Description',
+      description:
+        'Explore detailed cooking reports to optimize your culinary operations. Gain insights into ingredient usage, recipe performance, and cooking techniques. Enhance menu planning, improve kitchen efficiency, and deliver exceptional culinary experiences to your customers.',
       image: '../../imag-1.png',
-      url: '/cooking-reports',
+      url: '/3182931',
+      reportsData: [
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+      ],
     },
     {
       id: 'tip-reports',
       name: 'Tip Reports',
-      description: 'Product Description',
+      description:
+        'Discover insightful tip reports to optimize your service and enhance customer satisfaction. Dive into data-driven tips analysis to understand customer preferences, improve staff performance, and boost revenue through exceptional service experiences.',
       image: '../../imag-2.png',
       url: '/tip-reports',
+      reportsData: [
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+      ],
     },
     {
       id: 'ingredient-reports',
       name: 'Ingredients Reports',
-      description: 'Product Description',
+      description:
+        'Unlock the power of ingredients reports to optimize your kitchen operations and elevate culinary creations. Explore comprehensive insights into ingredient usage, sourcing, costs, and trends to enhance menu planning, manage inventory effectively, and ensure culinary excellence.',
       image: '../../imag-3.png',
       url: '/ingredient-reports',
+      reportsData: [
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+      ],
     },
     {
       id: 'revenue-reports',
       name: 'Revenue Reports',
-      description: 'Product Description',
+      description: `Harness the power of revenue reports to gain deep insights into your business's financial performance and drive strategic decision-making. Analyze revenue streams, track sales trends, identify growth opportunities, and optimize pricing strategies to maximize profitability and sustainable growth.`,
       image: '../../imag-4.png',
       url: '/revenue-reports',
+      reportsData: [
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+      ],
     },
     {
       id: 'route-reports',
       name: 'Route Reports',
-      description: 'Product Description',
+      description: `Optimize your business's logistics and delivery operations with route reports. Gain valuable insights into delivery routes, efficiency metrics, and transportation costs. Streamline your delivery processes, improve delivery times, and enhance customer satisfaction with data-driven route optimization strategies.`,
       image: '../../imag-5.png',
       url: '/route-reports',
+      reportsData: [
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+        {
+          id: '3182931',
+          name: 'Ingredient Analysis',
+          description:
+            'Delve into ingredient analysis reports to understand ingredient trends.',
+          image: '../../imag-1.png',
+          url: '/3182931/ingredient-analysis',
+        },
+        {
+          id: '318293321',
+          name: 'Recipe Performance',
+          description:
+            'Analyze recipe performance metrics to identify popular dishes.',
+          image: '../../imag-2.png',
+          url: '/318293231/recipe-performance',
+        },
+        {
+          id: '3182935321',
+          name: 'Cooking Techniques',
+          description: 'Explore cooking techniques reports to master.',
+          image: '../../imag-3.png',
+          url: '/3182931/cooking-techniques',
+        },
+        {
+          id: '3182931',
+          name: 'Menu Optimization',
+          description: 'Optimize your menu with insightful reports on dish.',
+          image: '../../imag-4.png',
+          url: '/3182931/menu-optimization',
+        },
+      ],
     },
   ];
 
@@ -50,7 +363,17 @@ export class ReportsService {
 
   getReportsById(ids: string[]) {
     return Promise.resolve(
-      this.getData().filter((report) => ids.includes(report.id))
+      this.getData().flatMap((report) => {
+        if (ids.includes(report.id)) {
+          if (report.reportsData) {
+            return report.reportsData;
+          } else {
+            return [report];
+          }
+        } else {
+          return [];
+        }
+      })
     );
   }
 }
