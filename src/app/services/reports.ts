6230a3299005ddd651891f7569cb104d4ae30dbf
William James <sabirnawaz59@gmail.com>
2024-05-01 18:32:06 +0500

    some content stuffs done

---
+++ src/app/services/reports.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ReportssService {
+  private apiUrl =
+    'https://raw.githubusercontent.com/Sabir59/dashboard-api/main/reports.json';
+
+  constructor(private http: HttpClient) {}
+
+  async getReports() {
+    try {
+      const data = await this.http.get<any>(this.apiUrl).toPromise();
+      return data;
+      console.log('Fetched reports data:', data);
+    } catch (error) {
+      console.error('Error fetching reports:', error);
+    }
+  }
+}
